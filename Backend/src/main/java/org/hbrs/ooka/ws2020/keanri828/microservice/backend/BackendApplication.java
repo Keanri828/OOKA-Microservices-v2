cf956a39b5e9a4553b41d1655d3c83fc7557deb7
Fiona Brücher <fyobru@gmail.com>
2021-01-14 01:46:01 +0100

    initial commit

---
+++ Backend/src/main/java/org/hbrs/ooka/ws2020/keanri828/microservice/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package org.hbrs.ooka.ws2020.keanri828.microservice.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
